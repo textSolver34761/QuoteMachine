1ed5e70d141954587d4f67b5d4c9eba51e1d34d8
textSolver34761 <pradonbenjamin@gmail.com>
2022-05-17 16:07:02 +0200

    update

--- src/main/java/com/quotemachine/QuoteMachine/controller/QuoteController.java
+++ src/main/java/com/quotemachine/QuoteMachine/controller/QuoteController.java
@@ -1,6 +1,11 @@
 package com.quotemachine.QuoteMachine.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -8,10 +13,22 @@ import com.quotemachine.QuoteMachine.model.Quotes;
 
 @Controller
 public class QuoteController {
-	@PostMapping("/")
-	public String showForm(@ModelAttribute("Quotes") Quotes quotes) {
+	
+	List<Quotes> quoteList = new ArrayList<Quotes>();
+	
+	@GetMapping("/")	
+	public String showForm(Model model) {
+		Quotes quotes = new Quotes();
+		model.addAttribute("Quotes", quotes);
 		quotes.setAuthor("");
 		quotes.setQuote("");
+		return "index";
+	}
+	
+	@PostMapping("/")
+	public String display(@ModelAttribute("Quotes") Quotes quotes) {
+		quoteList.add(quotes);
+		System.out.println("LIST " + quotes);
 		return "index";
 	}
 }
